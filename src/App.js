6e3ec94f2e01a8f5896b19d411200f384507c9e7
julinorieg <julieta.noriega30@gmail.com>
2023-06-08 22:41:53 -0300

    img

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from './components/NavBar/NavBar.js';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import ItemCount from "./components/ItemCount/ItemCount";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <NavBar/>
-    <ItemListContainer greeting={"Tu Super Online!"}/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/categoryId" element={<ItemListContainer />} />
+          <Route path="/Item/:ItemId" element={<ItemDetailContainer />} />
+          <Route path="*" element={<h1> 404 NOT FOUND </h1>} />
+        </Routes>
+      </BrowserRouter>
+
+      <ItemListContainer greeting={"¡Tu Super Online!"} />
+      <ItemDetailContainer />
     </div>
   );
 }
